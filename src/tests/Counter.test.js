e7858f7c2b404bca320ef99fab45de50242fda3e
ChandlerWiggins <chandlerwig@gmail.com>
2024-04-08 17:40:32 -0400

    implemented unit tests

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,37 @@
-// import necessary react testing library helpers here
-// import the Counter component here
+import React from 'react';
+import { render, screen, fireEvent, cleanup } from '@testing-library/react';
+import Counter from '../components/Counter';
 
 beforeEach(() => {
-  // Render the Counter component here
+  render(<Counter />);
 })
 
 test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  const countDisplay = screen.getByTestId('count');
+  expect(countDisplay).toHaveTextContent('0');
 });
 
 test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+  const countValue = screen.getByTestId('count');
+  expect(countValue).toHaveTextContent('0');
 });
 
 test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  const incrementButton = screen.getByRole('button', { name: '+' });
+  fireEvent.click(incrementButton);
+  const countValue = screen.getByTestId('count');
+  expect(countValue).toHaveTextContent('1');
 });
 
 test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  const incrementButton = screen.getByRole('button', { name: '+' });
+  const decrementButton = screen.getByRole('button', { name: '-' });
+
+  // First, increment the count to avoid going negative, unless negative is handled
+  fireEvent.click(incrementButton);
+  fireEvent.click(incrementButton); // Increment twice to test decrementing next
+
+  fireEvent.click(decrementButton);
+  const countValue = screen.getByTestId('count');
+  expect(countValue).toHaveTextContent('1');
 });
